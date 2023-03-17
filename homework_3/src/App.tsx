c73c91fa168d994675486e80915cbf7d5e1fa8a1
bubblegone <bubblegonex@gmail.com>
2023-03-17 15:08:14 +0100

    fix homework 3

--- homework_3/src/App.tsx
+++ homework_3/src/App.tsx
@@ -1,43 +1,19 @@
 import { useState } from 'react';
+
 function GetDegree() {
     const [number, setNumber] = useState(0);
     const [degree, setDegree] = useState(0);
     const [sum, setSum] = useState(0);
 
-    function increment1() {
-        setNumber(number + 1);
-    }
-
-    function decrement1() {
+    function decrementNumber() {
         if (number > 0) {
             setNumber(number - 1);
         }
     }
-    function reset1() {
-        setNumber(0);
-    }
 
-    function increment2() {
-        setDegree(degree + 1);
-    }
-
-    function decrement2() {
+    function decrementDegree() {
         if (degree > 0) {
             setDegree(degree - 1);
-        }
-    }
-    function reset2() {
-        setDegree(0);
-    }
-
-    function sumDegree() {
-        let a = 1;
-        if (degree > a) {
-            a++;
-            setSum(number ** degree);
-        }
-        if (degree === 1) {
-            setSum(number);
         }
     }
 
@@ -47,16 +23,16 @@ function GetDegree() {
                 {number} ^ {degree} = {sum}
             </div>
             <div>
-                <button onClick={increment1}>Inc</button>
-                <button onClick={decrement1}>Dec</button>
-                <button onClick={reset1}>Reset</button>
+                <button onClick={() => setNumber(number + 1)}>Inc</button>
+                <button onClick={decrementNumber}>Dec</button>
+                <button onClick={() => setNumber(0)}>Reset</button>
             </div>
             <div>
-                <button onClick={increment2}>Inc</button>
-                <button onClick={decrement2}>Dec</button>
-                <button onClick={reset2}>Reset</button>
+                <button onClick={() => setDegree(degree + 1)}>Inc</button>
+                <button onClick={decrementDegree}>Dec</button>
+                <button onClick={() => setDegree(0)}>Reset</button>
             </div>
-            <button className='bnt-push' onClick={sumDegree}>
+            <button className='bnt-push' onClick={() => setSum(number ** degree)}>
                 push
             </button>
         </div>
