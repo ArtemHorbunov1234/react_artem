b9678ab91bcf5cad193abb19956648d1e54b4f2c
bubblegone <bubblegonex@gmail.com>
2023-03-30 18:15:48 +0200

    fix homework 6

--- homework_6/src/App.tsx
+++ homework_6/src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
+
 function InputPushText() {
-    const [names, setName] = useState('');
+    const [name, setName] = useState('');
     const [save, setSave] = useState<string[]>([]);
     function inputPush() {
-        setSave([names, ...save]);
+        setSave([name, ...save]);
         setName('');
     }
 
     return (
         <div>
-            <input type='text' value={names} onChange={e => setName(e.target.value)} />
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
             <button onClick={() => inputPush()}>Push</button>
-            <p>{save}</p>
+            <p>
+                {save.map((name, index) => (
+                    <div key={index}>{name}</div>
+                ))}
+            </p>
         </div>
     );
 }
