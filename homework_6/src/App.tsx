6ac24e46cdedf1c0cb3eb3d646a5cc8135834d93
Artem <kolyasik124@ukr.net>
2023-03-30 19:08:42 +0300

    1

--- homework_6/src/App.tsx
+++ homework_6/src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 function InputPushText() {
-    const [names, setName] = useState<string[]>([]);
+    const [names, setName] = useState('');
     const [save, setSave] = useState<string[]>([]);
     function inputPush() {
-        setSave([...names, ...save]);
-        setName(['']);
+        setSave([names, ...save]);
+        setName('');
     }
 
     return (
         <div>
-            <input type='text' value={names} onChange={e => setName([e.target.value])} />
+            <input type='text' value={names} onChange={e => setName(e.target.value)} />
             <button onClick={() => inputPush()}>Push</button>
             <p>{save}</p>
         </div>
