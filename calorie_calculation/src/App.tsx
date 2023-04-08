0dfc9976f17366ef7bd518a059a2c4884a06be1a
bubblegone <bubblegonex@gmail.com>
2023-04-08 17:35:47 +0200

    fix fix

--- calorie_calculation/src/App.tsx
+++ calorie_calculation/src/App.tsx
@@ -1,35 +1,49 @@
 import { useState } from 'react';
 function CalculationsCalorie() {
-    const [calorie, setCalorie] = useState(0);
-    const [protein, setProtein] = useState(0);
-    const [fat, setFat] = useState(0);
-    const [carbs, setCarbs] = useState(0);
+    const [calorie, setCalorie] = useState<number | ''>('');
+    const [protein, setProtein] = useState<number | ''>('');
+    const [fat, setFat] = useState<number | ''>('');
+    const [carbs, setCarbs] = useState<number | ''>('');
     const [totalProtein, setTotalProtein] = useState(0);
     const [totalFat, setTotalFat] = useState(0);
     const [totalCarbs, setTotalCarbs] = useState(0);
     function totalCalculation() {
+        if (protein === '' || calorie === '' || carbs === '' || fat === '') {
+            return;
+        }
+
         setTotalProtein(((protein / 100) * calorie) / 4);
         setTotalFat(((fat / 100) * calorie) / 9);
         setTotalCarbs(((carbs / 100) * calorie) / 4);
     }
+
+    function convertString(value: string) {
+        const numValue = Number(value);
+        if (numValue === 0) {
+            return '';
+        } else {
+            return numValue;
+        }
+    }
+
     return (
         <div className='header'>
             <label className='header--text'>
-                Pcalorie:
-                <input type='number' value={calorie} onChange={e => setCalorie(Number(e.target.value))} />%
+                calorie:
+                <input type='number' value={calorie} onChange={e => setCalorie(convertString(e.target.value))} />
             </label>
             <label className='header--text'>
-                Protein:{totalProtein}grams
-                <input type='number' value={protein} onChange={e => setProtein(Number(e.target.value))} />%
+                protein:{totalProtein.toFixed(2)}grams
+                <input type='number' value={protein} onChange={e => setProtein(convertString(e.target.value))} />%
             </label>
             <label className='header--text'>
-                Fat:{totalFat}grams
-                <input type='number' value={fat} onChange={e => setFat(Number(e.target.value))} />%
+                fat:{totalFat.toFixed(2)}grams
+                <input type='number' value={fat} onChange={e => setFat(convertString(e.target.value))} />%
             </label>
 
             <label className='header--text'>
-                Carbs:{totalCarbs}grams
-                <input type='number' value={carbs} onChange={e => setCarbs(Number(e.target.value))} />%
+                carbs:{totalCarbs.toFixed(2)}grams
+                <input type='number' value={carbs} onChange={e => setCarbs(convertString(e.target.value))} />%
             </label>
             <button onClick={() => totalCalculation()}>Calculation</button>
         </div>
