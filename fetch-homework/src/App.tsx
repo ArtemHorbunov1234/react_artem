c7d07cfaccc61b75d8494b434c41bc1d528ed511
Artem <kolyasik124@ukr.net>
2023-04-13 18:08:16 +0300

    homework

--- fetch-homework/src/App.tsx
+++ fetch-homework/src/App.tsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from 'react';
+function AddPost() {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState<number | ''>('');
+    const [img, setImg] = useState('');
 
-function App() {
-  const [count, setCount] = useState(0)
+    function convertString(value: string) {
+        const numValue = Number(value);
+        if (numValue === 0) {
+            return '';
+        } else {
+            return numValue;
+        }
+    }
+    function postShop() {
+        const newPost = {
+            title: title,
+            price: price,
+            url: img,
+        };
+        fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            body: JSON.stringify(newPost),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className='post--input'>
+            <label>
+                Title:
+                <input type='text' value={title} onChange={e => setTitle(e.target.value)} />
+            </label>
+            <label>
+                Price:
+                <input type='number' value={price} onChange={e => setPrice(convertString(e.target.value))} />
+            </label>
+            <label>
+                Url:
+                <input type='text' value={img} onChange={e => setImg(e.target.value)} />
+            </label>
+
+            <button className='post_btn--push' onClick={postShop}>
+                Push
+            </button>
+        </div>
+    );
+}
+type List = {
+    id: number;
+    title: string;
+    price: number;
+    url: string;
+};
+function PostList() {
+    const [posts, setPosts] = useState<List[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/posts')
+            .then(response => response.json())
+            .then(data => setPosts(data));
+    }, []);
+
+    function deletePost(id: number) {
+        fetch(`http://localhost:3000/posts/${id}`, {
+            method: 'DELETE',
+        }).then(res => console.log(res));
+    }
+
+    return (
+        <div className='post__container'>
+            {posts.map(post => (
+                <div key={post.id}>
+                    <div>{post.title}</div>
+                    <div>Price: {post.price} UAH</div>
+                    <div>
+                        <img className='post--img' src={post.url} />
+                    </div>
+                    <button className='post--btn' onClick={() => deletePost(post.id)}>
+                        Del
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default App
+function App() {
+    return (
+        <div className='App'>
+            <AddPost />
+            <PostList />
+        </div>
+    );
+}
+
+export default App;
