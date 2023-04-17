0b42b5cc255fbc8eaffc8ffff12e054b9e879e88
bubblegone <bubblegonex@gmail.com>
2023-04-17 21:52:30 +0200

    Update with react query

--- fetch-homework/src/App.tsx
+++ fetch-homework/src/App.tsx
@@ -1,4 +1,7 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { queryClient } from './queryClient';
+
 function AddPost() {
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState<number | ''>('');
@@ -12,13 +15,19 @@ function AddPost() {
             return numValue;
         }
     }
+
+    const createMutation = useMutation({
+        mutationFn: postShop,
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['getAllPosts'] }),
+    });
+
     function postShop() {
         const newPost = {
             title: title,
             price: price,
             url: img,
         };
-        fetch('http://localhost:3000/posts', {
+        return fetch('http://localhost:3000/posts', {
             method: 'POST',
             body: JSON.stringify(newPost),
             headers: {
@@ -42,43 +51,49 @@ function AddPost() {
                 <input type='text' value={img} onChange={e => setImg(e.target.value)} />
             </label>
 
-            <button className='post_btn--push' onClick={postShop}>
+            <button className='post_btn--push' onClick={() => createMutation.mutate()}>
                 Push
             </button>
         </div>
     );
 }
-type List = {
+
+type Post = {
     id: number;
     title: string;
     price: number;
     url: string;
 };
+
 function PostList() {
-    const [posts, setPosts] = useState<List[]>([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3000/posts')
-            .then(response => response.json())
-            .then(data => setPosts(data));
-    }, []);
-
-    function deletePost(id: number) {
-        fetch(`http://localhost:3000/posts/${id}`, {
-            method: 'DELETE',
-        }).then(res => console.log(res));
+    function getAllPosts(): Promise<Post[]> {
+        return fetch('http://localhost:3000/posts').then(response => response.json());
     }
+
+    const { data: posts } = useQuery({
+        queryKey: ['getAllPosts'],
+        queryFn: getAllPosts,
+        initialData: [],
+    });
+
+    const deleteMutation = useMutation({
+        mutationFn: (id: number) =>
+            fetch(`http://localhost:3000/posts/${id}`, {
+                method: 'DELETE',
+            }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['getAllPosts'] }),
+    });
 
     return (
         <div className='post__container'>
             {posts.map(post => (
-                <div key={post.id}>
+                <div key={post.id} onClick={() => deleteMutation.mutate(post.id)}>
                     <div>{post.title}</div>
                     <div>Price: {post.price} UAH</div>
                     <div>
                         <img className='post--img' src={post.url} />
                     </div>
-                    <button className='post--btn' onClick={() => deletePost(post.id)}>
+                    <button className='post--btn' onClick={() => deleteMutation.mutate(post.id)}>
                         Del
                     </button>
                 </div>
