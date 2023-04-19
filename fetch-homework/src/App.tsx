f64902cfe47cae69c30964371c2a6a1fd33e0dcb
Artem <kolyasik124@ukr.net>
2023-04-19 21:57:42 +0300

    1

--- fetch-homework/src/App.tsx
+++ fetch-homework/src/App.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { queryClient } from './queryClient';
 
 function AddPost() {
@@ -50,7 +50,6 @@ function AddPost() {
                 Url:
                 <input type='text' value={img} onChange={e => setImg(e.target.value)} />
             </label>
-
             <button className='post_btn--push' onClick={() => createMutation.mutate()}>
                 Push
             </button>
@@ -66,10 +65,11 @@ type Post = {
 };
 
 function PostList() {
+    const [comment, setComment] = useState('');
+    const [hiddenComment, setHiddenComment] = useState(false);
     function getAllPosts(): Promise<Post[]> {
         return fetch('http://localhost:3000/posts').then(response => response.json());
     }
-
     const { data: posts } = useQuery({
         queryKey: ['getAllPosts'],
         queryFn: getAllPosts,
@@ -84,10 +84,50 @@ function PostList() {
         onSuccess: () => queryClient.invalidateQueries({ queryKey: ['getAllPosts'] }),
     });
 
+    const commentsMutation = useMutation({
+        mutationFn: postComment,
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['getAllComments'] }),
+    });
+
+    function postComment() {
+        const newComment = {
+            comment: comment,
+        };
+        return fetch('http://localhost:3000/comments', {
+            method: 'POST',
+            body: JSON.stringify(newComment),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    // type Comment = {
+    //     comment: string;
+    //     id: number;
+    // };
+    // function getAllComments(): Promise<Comment[]> {
+    //     return fetch('http://localhost:3000/comments').then(response => response.json());
+    // }
+    // const { data: comments } = useQuery({
+    //     queryKey: ['getAllComments'],
+    //     queryFn: getAllComments,
+    //     initialData: [],
+    // });
+    // let textComment;
+    // if (hiddenComment) {
+    //     textComment = (
+    //         <div>
+
+    //         </div>
+    //     );
+    // } else if (!hiddenComment) {
+    //     return;
+    // }
+
     return (
         <div className='post__container'>
             {posts.map(post => (
-                <div key={post.id} onClick={() => deleteMutation.mutate(post.id)}>
+                <div key={post.id}>
                     <div>{post.title}</div>
                     <div>Price: {post.price} UAH</div>
                     <div>
@@ -96,6 +136,19 @@ function PostList() {
                     <button className='post--btn' onClick={() => deleteMutation.mutate(post.id)}>
                         Del
                     </button>
+                    {comments.map(comment => (
+                        <div key={comment.id}>
+                            <div>{comment.comment}</div>
+                        </div>
+                    ))}
+                    <textarea
+                        value={post.id}
+                        placeholder='Comment'
+                        className='post__comment'
+                        onChange={e => setComment(e.target.value)}
+                    ></textarea>
+
+                    <button onClick={() => commentsMutation.mutate()}>Comment Push</button>
                 </div>
             ))}
         </div>
