0f02eb6dd5f6070dfcecd2a3e252fe7fb581f7ca
bubblegone <bubblegonex@gmail.com>
2023-04-27 19:01:08 +0200

    fix hook-form

--- hook-form/src/App.tsx
+++ hook-form/src/App.tsx
@@ -19,22 +19,22 @@ function FormInput() {
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='input-registration '>
             <label>Email:</label>
-            <input {...register('example')} placeholder='Email' minLength={6} />
-            {errors.example && <span>Fill in the email field</span>}
+            <input {...register('example', { minLength: 6, required: true })} placeholder='Email' />
+            {errors.example?.type === 'required' && <span>Fill in the email field</span>}
+            {errors.example?.type === 'minLength' && <span>Email must be 6 chars minimum</span>}
 
             <label>Password:</label>
 
             <input
-                {...register('exampleRequired', { required: true })}
+                {...register('exampleRequired', { required: true, minLength: 5 })}
                 placeholder='Password'
                 type={hidden ? 'password' : 'text'}
-                minLength={5}
             />
             <button className='btn-show' onClick={() => setHidden(!hidden)}>
                 SHOW
             </button>
-
-            {errors.exampleRequired && <span>Fill in the password field</span>}
+            {errors.exampleRequired?.type === 'required' && <span>Fill in the password field</span>}
+            {errors.exampleRequired?.type === 'minLength' && <span>Password can't be less than 5 letters</span>}
 
             <button type='submit'> Push</button>
         </form>
